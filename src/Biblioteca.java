efcee927ac64884ae367ed6a2d263326cce4cb24
IvanG <ivn.gtzs@gmail.com>
2024-10-21 20:20:26 -0600

    Clase BibliotecaImpl y correccion interfaz Biblioteca

--- src/Biblioteca.java
+++ src/Biblioteca.java
@@ -1,11 +1,11 @@
 import java.util.List;
 
 public interface Biblioteca {
-    String agregarLibro(Libro libro);
-    String eliminarLibro(String isbn);
-    List<Libro> listarLibros();
-    String registrarUsuario(Usuario usuario);
-    List<Usuario> listarUsuario();
-    List<Libro> buscarLibroXTitulo(String titulo);
+    String agregarLibro(List<Libro> listaLibro, Libro libro);
+    String eliminarLibro(List<Libro> listaLibro,String isbn);
+    List<Libro> listarLibros(List<Libro> listaLibro);
+    String registrarUsuario(List<Usuario> listaUsuario, Usuario usuario);
+    List<Usuario> listarUsuario(List<Usuario> listaUsuario);
+    List<Libro> buscarLibroXTitulo(List<Libro> listaLibro, String titulo);
 
 }
---
+++ src/BibliotecaImpl.java
@@ -0,0 +1,34 @@
+import java.util.List;
+
+public class BibliotecaImpl implements Biblioteca{
+
+    @Override
+    public String agregarLibro(List<Libro> listaLibro, Libro libro) {
+        return null;
+    }
+
+    @Override
+    public String eliminarLibro(List<Libro> listaLibro, String isbn) {
+        return null;
+    }
+
+    @Override
+    public List<Libro> listarLibros(List<Libro> listaLibro) {
+        return null;
+    }
+
+    @Override
+    public String registrarUsuario(List<Usuario> listaUsuario, Usuario usuario) {
+        return null;
+    }
+
+    @Override
+    public List<Usuario> listarUsuario(List<Usuario> listaUsuario) {
+        return null;
+    }
+
+    @Override
+    public List<Libro> buscarLibroXTitulo(List<Libro> listaLibro, String titulo) {
+        return null;
+    }
+}
