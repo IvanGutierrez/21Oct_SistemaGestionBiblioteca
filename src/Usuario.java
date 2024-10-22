f19e0b3cf75a646a6d1df232963a3d768b2c1e07
IvanG <ivn.gtzs@gmail.com>
2024-10-21 20:00:44 -0600

    Clase Usuario

---
+++ src/Usuario.java
@@ -0,0 +1,34 @@
+public class Usuario {
+    public String nombre;
+    public String id;
+
+    public Usuario(){}
+    public Usuario(String nombre, String id) {
+        this.nombre = nombre;
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" +
+                "nombre='" + nombre + '\'' +
+                ", id='" + id + '\'' +
+                '}';
+    }
+}
